56b58d732557b3b8af6135666487b34ff17ec167
Philipp Schroeppel <69214052+pns1123@users.noreply.github.com>
2023-12-04 17:25:05 +0100

    prepare day05

---
+++ solutions/day05/app/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import           Lib
+
+main :: String -> IO ()
+main = printSol "filePath"
---
+++ solutions/day05/src/Lib.hs
@@ -0,0 +1,9 @@
+module Lib
+    ( printSol
+    ) where
+
+printSol :: String -> IO ()
+printSol inputFilePath = do
+    content <- readFile inputFilePath
+    --print $ solveQ1 content
+    --print $ solveQ2 content
