5414607e8034b1b75351be1199ac0aaee32c884c
Philipp Schroeppel <69214052+pns1123@users.noreply.github.com>
2023-12-09 11:22:03 +0100

    add day09

--- solutions/day09/src/Lib.hs
+++ solutions/day09/src/Lib.hs
@@ -1,6 +1,43 @@
 module Lib
-    ( someFunc
+    ( printSol
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+printSol :: String -> IO ()
+printSol inputPath = do
+    content <- readFile inputPath
+    print $ solveQ1 content
+    print $ solveQ2 content
+
+solveQ1 :: String -> Int
+solveQ1 content = sum $ iterateSeq <$> sequences
+                  where sequences = getSequences content
+
+solveQ2 :: String -> Int
+solveQ2 content = sum $ iterateSeqStart <$> sequences
+                  where sequences = getSequences content
+
+getSequences :: String -> [[Int]]
+getSequences content = map read . words <$> lines content
+
+diffSeq :: [Int] -> [Int]
+diffSeq xs = [a-b | (b,a) <- zip xs (tail xs)]
+
+aggDiffs :: [Int] -> [Int] -> [Int]
+aggDiffs xs ys | all (==0) xs = ys
+               | otherwise  = aggDiffs d (last d : ys)
+               where d = diffSeq xs
+
+iterateSeq :: [Int] -> Int
+iterateSeq [] = 0
+iterateSeq xs = sum $ aggDiffs xs [last xs]
+
+
+aggDiffsStart :: [Int] -> [Int] -> [Int]
+aggDiffsStart xs ys | all (==0) xs = ys
+                    | otherwise  = aggDiffsStart d (head d : ys)
+                    where d = diffSeq xs
+
+iterateSeqStart :: [Int] -> Int
+iterateSeqStart [] = 0
+-- agg = b - x <=> x = b - agg
+iterateSeqStart xs = foldl (flip (-)) 0 (aggDiffsStart xs [head xs])
