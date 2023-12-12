926bff9647661a24739808e90ff8fc30359b51b0
Philipp Schroeppel <69214052+pns1123@users.noreply.github.com>
2023-12-13 00:37:50 +0100

    add day12 part 1

--- solutions/day12/src/Lib.hs
+++ solutions/day12/src/Lib.hs
@@ -1,6 +1,35 @@
 module Lib
-    ( someFunc
+    ( printSol
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad (guard)
+import           Data.Text     (pack, splitOn, unpack)
+
+printSol :: IO ()
+printSol = do
+    content <- readFile "inputs/test_input"
+    let preproc = [(s, getCounts c) | [s,c] <- words <$> lines content]
+
+    print $ [ length $ filter (validRecord $ getCounts c) $ solve (getCounts c) s
+            | [s,c] <- words <$> lines content]
+
+solve :: [Int] -> String -> [String]
+solve counts xs = do
+    b <- branch xs
+    guard (length (filter (=='#') b) <= sum counts)
+    if '?' `notElem` b then return b else solve counts b
+
+getCounts :: String -> [Int]
+getCounts xs = read . unpack <$> splitOn (pack ",") (pack xs)
+
+validRecord :: [Int] -> String -> Bool
+validRecord ref xs = ref == [(length . unpack) x
+                            | x <- splitOn (pack ".") (pack xs), x /= pack ""]
+
+branch :: String -> [String]
+branch = _branch []
+
+_branch :: String -> String -> [String]
+_branch prefix [] = [prefix]
+_branch prefix (x:xs) | x == '?'  = [prefix ++ '.' : xs, prefix ++ '#' : xs]
+                      | otherwise = _branch (prefix ++ [x]) xs
